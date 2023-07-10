200268ff11b23f85ec1088a1328fe29e5f49e86a
Maryen <voujum@gmail.com>
2023-07-10 22:32:02 +0200

    Axios chats fetching

--- client/src/routes/chat-page.component.tsx
+++ client/src/routes/chat-page.component.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000';
+
+type User = {
+  name: string;
+  email: string;
+};
+
+type Chat = {
+  isGroupChat: boolean;
+  users: User[];
+  _id: string;
+  chatName: string;
+};
+
 const ChatPage = () => {
+  const [chats, setChats] = useState<Chat[]>([]);
+
+  const fetchChats = async () => {
+    const { data }: { data: Chat[] } = await axios.get(API_URL + '/api/chat');
+
+    setChats(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
   return (
     <ul>
+      {chats.map((chat, i) => (
+        <li key={i}>- {chat.chatName}</li>
+      ))}
     </ul>
   );
 };
