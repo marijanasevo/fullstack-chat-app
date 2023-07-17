46c1490b8843cd59f548fcc40abd876b105a8a35
Maryen <voujum@gmail.com>
2023-07-17 22:44:51 +0200

    Login frontend + fetch

--- client/src/components/authentication/login.component.tsx
+++ client/src/components/authentication/login.component.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios, { AxiosError } from 'axios';
+
 import {
   Button,
   FormControl,
@@ -7,15 +10,71 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from '@chakra-ui/react';
+
+import { toastOptions } from '../../utils/toast-options';
+
+const API_URL = 'http://localhost:3000';
+
+type UserData = {
+  name: string;
+  email: string;
+  password: string;
+  id: string;
+  token: string;
+  image: string;
+};
+
+interface LoginErrorResponse {
+  error: string;
+}
+
+type UserResponseType = UserData | LoginErrorResponse;
 
 function Login() {
   const [show, setShow] = useState<boolean>(false);
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const handleSubmit = async (): Promise<void> => {
+    if (!email || !password) {
+      toast({ ...toastOptions, title: 'Please fill in all the fields' });
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const { data } = await axios.post<UserResponseType>(
+        API_URL + '/api/users/login',
+        { email, password },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+
+      toast({ ...toastOptions, title: 'Login successful', status: 'success' });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      navigate('/chats');
+    } catch (err) {
+      const error = err as AxiosError<Error>;
+      console.log('Error during authentication', error);
+      if (axios.isAxiosError(error)) {
+        toast({
+          ...toastOptions,
+          title: 'Uh oh, something went wrong',
+          description:
+            error.response?.data.message || 'An unknown error occurred',
+        });
+      }
+    } finally {
+      setLoading(false);
+    }
+
+    return;
   };
 
   return (
@@ -46,7 +105,12 @@ function Login() {
         </InputGroup>
       </FormControl>
 
-      <Button bgColor='#C6F6D5' width='100%' mt={15} onClick={handleSubmit}>
+      <Button
+        isLoading={loading}
+        bgColor='#C6F6D5'
+        width='100%'
+        mt={15}
+        onClick={handleSubmit}>
         Sign Up
       </Button>
 
